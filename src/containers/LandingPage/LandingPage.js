1f83e9f58f87cc8b9ca8489a1a39d9819be67e64
Heriberto Uroza <heribertouroza@pursuit.org>
2020-02-17 18:14:38 -0500

    added styling for the landing page, will start working on the individual pages

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -13,7 +13,9 @@ class Landing extends React.Component {
             <>
                 <div className='container'>
                     <section>
-                        <Link to='/neumorphism' >Neumorphism</Link>
+                        <Link to='/neumorphism' 
+                            className='title'
+                        >Neumorphism</Link>
                         <Link to='/neumorphism'>
                             <img src='https://cdn.dribbble.com/users/1299148/screenshots/9680920/web_1920___1.png' 
                             alt='neumorphism_image' 
@@ -24,7 +26,9 @@ class Landing extends React.Component {
                     </section>
 
                     <section>
-                        <Link to='/transparency' >Transparency</Link>
+                        <Link to='/transparency' 
+                            className='title'
+                        >Transparency</Link>
                         <Link to='/transparency' >
                             <img src='https://cdn.freebiesbug.com/wp-content/uploads/2012/12/transparent-ui-kit-free-psd.jpg' 
                             alt='transparency_image' 
@@ -35,7 +39,10 @@ class Landing extends React.Component {
                     </section>
 
                     <section>
-                        <Link to='/twotone'>Two Tone Color</Link>
+                        <Link to='/twotone'
+                            className='title'>Two Tone Color
+                        </Link>
+                        
                         <Link to='/twotone'>
                             <img src='https://elements-cover-images-0.imgix.net/39dd919b-3270-4248-b9bd-d98d76f19c4c?auto=compress%2Cformat&fit=max&w=710&s=5fb0ebda277e660a12120b1dda8770d9'
                             alt='twotone_image'
