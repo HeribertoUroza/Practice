3e14d459f03efd1ea7e847f4dedf3b85c0b5092c
Heriberto Uroza <heribertouroza@pursuit.org>
2020-03-15 14:41:28 -0400

    updated file structure to better use context

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 // ---- PAGES
 import Landing from './containers/LandingPage/LandingPage';
-import NeuPage from './containers/NeuPage/NeuPage';
-import TranPage from './containers/TranPage/TranPage';
-import TwoTonePage from './containers/TwoTonePage/TwoTonePage';
-
 
 // ---- CSS
 import './styles/app.css';
@@ -17,9 +13,6 @@ function App() {
       <HashRouter>
         <Switch>
           <Route path='/' exact component={ Landing } />
-          <Route path='/neumorphism' exact component={ NeuPage } />
-          <Route path='/transparency' exact component={ TranPage } />
-          <Route path='/twotone' exact component={ TwoTonePage } />
         </Switch>
       </HashRouter>
     </>
--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -19,47 +19,7 @@ class Landing extends React.Component {
 
         return (
             <>
-                <div className='container'>
-                    <section>
-                        <Link to='/neumorphism'
-                            className='title'
-                        >Neumorphism</Link>
-                        <Link to='/neumorphism'>
-                            <img src='https://cdn.dribbble.com/users/1299148/screenshots/9680920/web_1920___1.png' 
-                            alt='neumorphism_image' 
-                            className='image'/>
-                        </Link>
-                        
-                        {/* INSERT IMAGE TO LINK TO PAGE */}
-                    </section>
 
-                    <section>
-                        <Link to='/transparency' 
-                            className='title'
-                        >Transparency</Link>
-                        <Link to='/transparency' >
-                            <img src='https://cdn.freebiesbug.com/wp-content/uploads/2012/12/transparent-ui-kit-free-psd.jpg' 
-                            alt='transparency_image' 
-                            className='image' />
-                        </Link>
-
-                        {/* INSERT IMAGE TO LINK TO PAGE */}
-                    </section>
-
-                    <section>
-                        <Link to='/twotone'
-                            className='title'>Two Tone Color
-                        </Link>
-                        
-                        <Link to='/twotone'>
-                            <img src='https://elements-cover-images-0.imgix.net/39dd919b-3270-4248-b9bd-d98d76f19c4c?auto=compress%2Cformat&fit=max&w=710&s=5fb0ebda277e660a12120b1dda8770d9'
-                            alt='twotone_image'
-                            className='image' />
-                        </Link>
-                        {/* INSERT IMAGE TO LINK TO PAGE */}
-                    </section>
-
-                </div>
             </>
         )
     }
