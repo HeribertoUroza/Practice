48debb7c56705ec3b417d6c558fa8a312423b037
Heriberto Uroza <heribertouroza@pursuit.org>
2020-03-22 18:21:25 -0400

    hiding api key

--- src/context/NavThemeContext.js
+++ src/context/NavThemeContext.js
@@ -10,7 +10,7 @@ export const navThemes = {
     },
 
     transparent: {
-        background: 'brown'
+        backgroundColor: 'brown'
     },
 
     twoTone: {
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -6,8 +6,8 @@ const RandomCocktail = () => {
         method: 'get',
         url: 'https://the-cocktail-db.p.rapidapi.com/random.php',
         headers: {
-            "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-            "x-rapidapi-key": "65bc37a623msh0f6c8445f54e3d2p16af79jsne6aea299b290"
+            "x-rapidapi-host": process.env.REACT_APP_xrapidapihost,
+            "x-rapidapi-key": process.env.REACT_APP_xrapidapikey
         }
     })
 }
