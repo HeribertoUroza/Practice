c8ecb0710951bdbe4b5132722b312be24a658327
Heriberto Uroza <heribertouroza@pursuit.org>
2020-05-23 19:51:34 -0400

    trying with anchor tags to style better

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -101,9 +101,9 @@ const Landing = () => {
                 </section>
 
                 <nav className='nav' style={navTheme}>
-                    <button className='nav-btn' onClick={toggleTheme} name='neumorphism' style={navTheme} >Neumorphism</button>
-                    <button className='nav-btn' onClick={toggleTheme} name='transparent' style={navTheme} >Transparent</button>
-                    <button className='nav-btn' onClick={toggleTheme} name='twoTone' style={navTheme} >Two Tone</button>
+                    <a className='nav-btn' onClick={toggleTheme} href='none' name='neumorphism' style={navTheme} >Neumorphism</a>
+                    <a className='nav-btn' onClick={toggleTheme} href='none' name='transparent' style={navTheme} >Transparent</a>
+                    <a className='nav-btn' onClick={toggleTheme} href='none' name='twoTone' style={navTheme} >Two Tone</a>
                 </nav>
             </div>
         </>
